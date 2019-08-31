967c060850993ac43e82dbfef497f5d55bd48777
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-08-30 22:09:56 -0400

    RomanNumbers project added

---
+++ romanNumbers/src/main/java/RomanConverter.java
@@ -0,0 +1,10 @@
+public class RomanConverter {
+    public int romanToDecimal(String romanNumber) {
+        int decimalNumber = 0;
+        int length = romanNumber.length();
+        for (int index = 0; index < length; index++) {
+            
+        }
+        return decimalNumber;        
+    }
+}
