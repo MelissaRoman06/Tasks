b813a35d89026b4c6c3988bfea89980b8b66852f
MelissaRoman06 <melissa.roman@fundacion-jala.org>
2019-09-05 00:03:18 -0400

    Comments added.

--- CalculadoraTest/src/main/java/Operations.java
+++ CalculadoraTest/src/main/java/Operations.java
@@ -1,3 +1,6 @@
+/**
+ * This enum class has all the possible operations that can be performed by the calculator.
+ */
 public enum Operations {
     ADD,
     SUBTRACT,
--- CalculadoraTest/src/main/java/OperatorSymbol.java
+++ CalculadoraTest/src/main/java/OperatorSymbol.java
@@ -1,8 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class has a hash map used to parse Operations enum to corresponding key symbol.
+ */
 public class OperatorSymbol {
     private Map<Operations, String> operators;
+
+    /**
+     * Sets hash map content.
+     */
     public OperatorSymbol() {
         operators = new HashMap<Operations, String>() {{
             put(Operations.ADD, "+");
@@ -12,6 +19,11 @@ public class OperatorSymbol {
         }};
     }
 
+    /**
+     * Allows to get key symbol of the requested operation.
+     * @param operation - An option from Operations enum.
+     * @return - Key symbol which corresponds to operation input.
+     */
     public String getKeySymbol(Operations operation) {
         return operators.get(operation);
     }
