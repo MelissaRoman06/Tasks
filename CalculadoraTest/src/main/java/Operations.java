cb90b6e343e872118632e8484b35be4683b69863
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-09-04 22:54:35 -0400

    Running without page class

---
+++ CalculadoraTest/src/main/java/Operations.java
@@ -0,0 +1,6 @@
+public enum Operations {
+    ADD,
+    SUBTRACT,
+    MULTIPLICATE,
+    DIVIDE
+}
---
+++ CalculadoraTest/src/main/java/OperatorSimbol.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class OperatorSimbol {
+    private Map<Operations, String> operators;
+    public OperatorSimbol() {
+        operators = new HashMap<Operations, String>() {{
+            put(Operations.ADD, "+");
+            put(Operations.SUBTRACT, "-");
+            put(Operations.MULTIPLICATE, "×");
+            put(Operations.DIVIDE, "÷");
+        }};
+    }
+
+    public String getKeySimbol(Operations operation) {
+        return operators.get(operation);
+    }
+}
